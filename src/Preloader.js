6d71af06b8f18a7504e24c95099eda7742df43b9
mofazil17 <mofazil1717@gmail.com>
2021-10-15 10:10:47 +0530

    1 binaries uploaded

--- src/Preloader.js
+++ src/Preloader.js
@@ -9,7 +9,7 @@ export default function Preloader() {
         <img src={logo} class="svg" alt="Google Developer Student Clubs logo" width='300px'/>
       </div>
       <div clasSName="loading">
-        
+        <img src="https://cdn.jsdelivr.net/gh/mofazil17/display-data-in-table-using-react@master/src/Spinner-3.gif" alt="hello"/>
       </div>
     </div>
   );
